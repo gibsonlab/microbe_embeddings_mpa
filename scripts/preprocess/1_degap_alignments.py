0f832c1d17b6a305a0171760ce6ce32ed7b8395e
Younhun Kim <ykim78@bwh.harvard.edu>
2025-11-11 14:13:46 -0500

    updated step 1 to directly write to a concatenated BGZ-compressed file.

--- scripts/preprocess/1_degap_alignments.py
+++ scripts/preprocess/1_degap_alignments.py
@@ -54,6 +54,6 @@ def main(
 
 if __name__ == "__main__":
     main(
-        Path("/media/youn/data/projects/mpa_data/PhyloPhlAn_output/msas"),
-        Path("/media/youn/data/projects/mpa_data/processed")
+        Path("/data/cctm/youn/metaphlan_dset/phylophlan_data/PhyloPhlAn_output/msas"),
+        Path("/data/cctm/youn/metaphlan_dset/phylophlan_data/processed")
     )
