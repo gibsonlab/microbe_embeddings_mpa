63ee5308e47b1adf402c613e9facf70b70b1b06e
Younhun Kim <ykim78@bwh.harvard.edu>
2025-11-11 15:56:06 -0500

    added embedding slurm scripts.

---
+++ scripts/2_embed/compute_embeddings.py
@@ -0,0 +1,196 @@
+"""
+Script for pre-computing evo embeddings for marker genes.
+"""
+import argparse
+from typing import *
+from pathlib import Path
+import json
+
+from tqdm import tqdm
+import h5py
+from pyfaidx import Fasta
+import torch
+import zstandard as zstd
+
+from gem.embeddings import GenomeEmbedding, EvoWrapper, DNABertSWrapper
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", dest="model_name", type=str)
+    parser.add_argument("-f", "--fasta", dest="fasta_path", type=str)
+    parser.add_argument("-sl", "--sgb-list", dest="sgb_subset_file", type=str)
+    parser.add_argument("-si", "--sgb-index-file", dest="sgb_marker_index_file", type=str)
+    parser.add_argument("-s", "--start", dest="start", type=int, help="the first index (1-indexed, inclusive) of the marker to embed.")
+    parser.add_argument("-e", "--end", dest="end", type=int, help="the final index (1-indexed, inclusive) of the marker to embed.")
+    parser.add_argument("-b", "--batch-size", dest="batch_size", type=int)
+    parser.add_argument("-o", "--out-dir", dest="out_dir", type=str)
+    parser.add_argument("-sz", "--shard-size", dest="shard_size", type=int)
+    return parser.parse_args()
+
+
+def compute_embedding(
+        sgb_subset: List[str],
+        sgb_marker_index: Dict[str, Dict],
+        fasta_path: Path,
+        create_embed_model: Callable[[], GenomeEmbedding],
+        h5_output_dir: Path,
+        batch_size: int,
+        shard_size: int,
+        **embed_kwargs
+):
+    """
+    :param create_embed_model:
+    :param fai_path:
+    :param start_idx: The start index (inclusive) for the marker to embed.
+    :param end_idx: The end index (exclusive) for the marker to embed.
+    :return:
+    """
+
+    fasta = Fasta(fasta_path)
+    marker_ids_subset = []
+    for sgb_id in sgb_subset:
+        marker_ids_subset += sgb_marker_index[sgb_id]['markers']
+
+    n_seqs = len(marker_ids_subset)
+    print("{} SGBs --> {} sequences.".format(len(sgb_subset), n_seqs))
+    print("Desired shard size is {} sequences.".format(shard_size))
+
+    import math
+    print("Target # shards = {}".format(math.ceil(n_seqs / shard_size)))
+    pbar = tqdm(total=n_seqs)
+
+    # divide dataframe into shards.
+    index_path = h5_output_dir / "index.tsv"
+
+    print("Loading embedding model.")
+    embedding_model = create_embed_model()
+    padding_embedding = embedding_model.embed_empty_sequence().cpu().float().numpy()
+    print("Got padding embedding of shape {}".format(padding_embedding.shape))
+
+    with open(index_path, "wt") as index_file:
+        index_file.write("Marker\tShard\n")
+        for shard_idx, shard_start in enumerate(range(0, n_seqs, shard_size)):
+            print(f"Opening shard #{shard_idx}")
+            marker_id_shard_subset = marker_ids_subset[shard_start:shard_start + shard_size]
+            compute_embedding_shard(
+                marker_id_shard_subset,
+                fasta,
+                batch_size,
+                embedding_model,
+                embed_kwargs,
+                h5_output_dir / f"shard-{shard_idx}.h5",
+                pbar,
+            )
+
+            print(f"Writing index for shard #{shard_idx}")
+            for marker_id in marker_ids_subset:
+                index_file.write(
+                    "{}\t{}\n".format(marker_id, shard_idx)
+                )
+
+
+def compute_embedding_shard(
+        marker_id_subset: List[str],
+        fasta: Fasta,
+        batch_size: int,
+        embedding_model: GenomeEmbedding,
+        embed_kwargs: dict,
+        h5_output_path: Path,
+        pbar: tqdm
+):
+    n_seqs_shard = len(marker_id_subset)
+
+    with h5py.File(h5_output_path, 'w') as h5_file:
+        # Next, embed the marker sequences in batches.
+        for batch_idx, _i in enumerate(range(0, n_seqs_shard, batch_size)):
+            print("[*] batch #{}: {} --> {}".format(batch_idx, _i, _i + batch_size))
+            marker_ids_batch = marker_id_subset[_i:_i + batch_size]
+            marker_seqs = [str(fasta[m_id]) for m_id in marker_ids_batch]
+
+            try:
+                batch_embeddings = embedding_model.embed_batch(marker_seqs, **embed_kwargs).cpu().float().numpy()  # shape (batch_len, embed_dim)
+                for marker_id, marker_embedding in zip(marker_ids_batch, batch_embeddings):  # each marker gets its own hdf5 entry.
+                    h5_file.create_dataset(marker_id, data=marker_embedding, compression='lzf')
+            except RuntimeError as e:
+                print("Encountered unexpected error while computing batched embedding. Reverting to unbatched mode for this batch.")
+                print("The error message was: {}".format(str(e)))
+                for marker_id, marker_seq in zip(marker_ids_batch, marker_seqs):
+                    try:
+                        marker_embedding = embedding_model.embed_sequence(marker_seq).cpu().float().numpy()
+                    except RuntimeError as e:
+                        print(f"For some reason, was unable to embed marker {marker_id} -- Skipping.")
+                        print("The error message was: {}".format(str(e)))
+                    else:
+                        h5_file.create_dataset(marker_id, data=marker_embedding, compression='lzf')
+
+            pbar.update(len(marker_ids_batch))
+
+
+def do_job(
+        model_name: str,
+        fasta_path: Path,
+        sgb_subset_file: Path,
+        sgb_marker_index_file: Path,
+        start_idx: int,
+        end_idx: int,
+        batch_size: int,
+        out_dir: Path,
+        shard_size: int,
+):
+    if model_name == 'evo':
+        model_fn = lambda: EvoWrapper(device='cuda')
+    elif model_name == 'dnabert-s':
+        model_fn = lambda: DNABertSWrapper(device='cuda')
+    else:
+        raise ValueError("Unknown model name {}".format(model_name))
+
+    assert out_dir.parent.exists(), f"Directory {out_dir.parent} does not exist."
+    out_dir.mkdir(exist_ok=True)
+
+    with open(sgb_subset_file, "rt") as f:
+        sgb_subset = list(l.strip() for l in f)
+
+    # Next, trim any of the necessary "_group" prefixes.
+    sgb_subset = [
+        s[:-len("_group")] if s.endswith("_group")
+        else s
+        for s in sgb_subset
+    ]
+    with zstd.open(sgb_marker_index_file, "rt") as f:
+        sgb_marker_index = json.load(f)
+
+    print(f"Subset contains {len(sgb_subset)} SGBs.")
+    print(f"Handling subset index {start_idx} through {end_idx}.")
+    compute_embedding(
+        sgb_subset=sgb_subset[start_idx:end_idx],
+        sgb_marker_index=sgb_marker_index,
+        fasta_path=fasta_path,
+        create_embed_model=model_fn,
+        h5_output_dir=out_dir,
+        batch_size=batch_size,
+        shard_size=shard_size
+    )
+
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    print("CUDA available devices: {}".format(
+        torch.cuda.device_count()
+    ))
+
+    start_idx = args.start-1  # 0-indexed, inclusive
+    end_idx = args.end  # 0-indexed, exclusive
+    do_job(
+        model_name=args.model_name,
+        fasta_path=args.fasta_path,
+        sgb_subset_file=Path(args.sgb_subset_file),
+        sgb_marker_index_file=Path(args.sgb_marker_index_file),
+        start_idx=start_idx,
+        end_idx=end_idx,
+        batch_size=args.batch_size,
+        out_dir=Path(args.out_dir),
+        shard_size=args.shard_size,
+    )
