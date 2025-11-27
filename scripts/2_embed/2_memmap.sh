54db87359eeff844a526e6837876ac7d11a1b4af
Younhun Kim <ykim78@bwh.harvard.edu>
2025-11-26 19:23:49 -0500

    updated 2_memmap.sh with slurm headings.

--- scripts/2_embed/2_memmap.sh
+++ scripts/2_embed/2_memmap.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --partition=bwh_comppath_long
+#SBATCH --ntasks=1
+#SBATCH --mem=80G
+#SBATCH --cpus-per-task=40
+#SBATCH --time=5-00:00:00
+#SBATCH --job-name=memmap_tensors
+#SBATCH --output=memmap_%A_%a.out
+#SBATCH --error=memmap_%A_%a.err
+
+# Note: this is a Slurm script, meant to be run on ErisTwo/ErisXDL (CPU only) compute nodes, but it can also be run as a bash script.
 set -e
 
 
@@ -6,5 +16,5 @@ embeddings=/data/cctm/youn/metaphlan_dset/embeddings/phylophlan_markers/evo
 python 2_memmap.py \
   --input-embed-dir "${embeddings}" \
   --out-memmap-dir "${embeddings}/memmap" \
-  --threads 12
+  --threads 30
 echo "Done."
