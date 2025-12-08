6cd1244ec672ff7e888ccee1c8ec0a4a954e478f
Younhun Kim <ykim78@bwh.harvard.edu>
2025-12-08 13:41:53 -0500

    enabled multithreading.

--- scripts/3_train/2_memmap.sh
+++ scripts/3_train/2_memmap.sh
@@ -7,19 +7,22 @@
 #SBATCH --job-name=memmap_dataset
 #SBATCH --output=memmap_%A_%a.out
 #SBATCH --error=memmap_%A_%a.err
+set -e
 
 
 echo "Memory-mapping: Train set"
 python dataset_memmap.py \
   --dataset-tsv "/data/cctm/youn/metaphlan_dset/model_training/train.tsv" \
   --embedding-dir "/data/cctm/youn/metaphlan_dset/embeddings/phylophlan_markers/evo" \
-  --memmap-dir "/data/cctm/youn/metaphlan_dset/model_training/memmmap_samples"
+  --memmap-dir "/data/cctm/youn/metaphlan_dset/model_training/memmmap_samples" \
+  --threads 8
 
 
 echo "Memory-mapping: Test set"
 python dataset_memmap.py \
   --dataset-tsv "/data/cctm/youn/metaphlan_dset/model_training/test.tsv" \
   --embedding-dir "/data/cctm/youn/metaphlan_dset/embeddings/phylophlan_markers/evo" \
-  --memmap-dir "/data/cctm/youn/metaphlan_dset/model_training/memmmap_samples"
+  --memmap-dir "/data/cctm/youn/metaphlan_dset/model_training/memmmap_samples" \
+  --threads 8
 
 echo "Done!"
