0a576971528cb8d8ade5f6c93c86f9eef582b9ae
Younhun Kim <ykim78@bwh.harvard.edu>
2025-12-19 18:01:53 -0500

    raised default num_workers.

--- scripts/3_train/4_train_model_evo.sh
+++ scripts/3_train/4_train_model_evo.sh
@@ -43,7 +43,7 @@ python train_model.py \
   --learning-rate "$learning_rate" \
   --batch-size "$batch_size" \
   --print-every 5 \
-  --workers 4 \
+  --workers 10 \
   --seed "$seed" \
   --use-auto-mixed-precision \
   --prefetch-factor 2 \
