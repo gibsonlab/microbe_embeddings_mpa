977f98e80f9c423c7f338b5bb184e1042fdeef69
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-07 14:30:02 -0500

    added joblib parallelization

--- scripts/3_train/1_train_test_split.py
+++ scripts/3_train/1_train_test_split.py
@@ -454,6 +454,7 @@ def test_train_split_asv_separation(
 if __name__ == "__main__":
     DATA_DIR = Path("/data/cctm/youn/metaphlan_dset/dataset")
     OUT_DIR = Path("/data/cctm/youn/metaphlan_dset/model_training")
+    print(f"Destination OUT_DIR: {OUT_DIR}")
 
     full_profile_tsv = DATA_DIR / "BlancoMiguezA_2023_profiles.tsv"
     metadata_tsv = DATA_DIR / "BlancoMiguezA_2023_metadata.tsv"
