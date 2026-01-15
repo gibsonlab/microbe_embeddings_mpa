af001e1e79648256f3a4d06b8d24f3714ee717a3
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-15 17:21:47 -0500

    bugfix

--- scripts/1_preprocess/dna_only/1_create_fasta.py
+++ scripts/1_preprocess/dna_only/1_create_fasta.py
@@ -21,7 +21,7 @@ def gather_markers(sgb_marker_file: Path, out_file: Path, seen_ids: Set[str]):
             assert gene_name.startswith("p") and len(gene_name) == 5, f"Expected to find gene name of the form `p<4-digit-id>`, but got `{gene_name}` instead."
             sgb_genome_name = sgb_marker_file.name[:-len(file_suffix)]
             seq = record.seq
-            new_record = SeqRecord(seq, id=f"dna:{gene_name}:{sgb_genome_name}", description="")
+            new_record = SeqRecord(seq, id=f"{gene_name}:{sgb_genome_name}", description="")
             if new_record.id in seen_ids:
                 print(f"[WARNING] {new_record.id} already in use. Skipping!")
             else:
