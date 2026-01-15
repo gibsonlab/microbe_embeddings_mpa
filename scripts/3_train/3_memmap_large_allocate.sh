779e89be39ba83907812efea9dfd43299fd9c068
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-15 15:14:21 -0500

    implemented large tensordict allocation.

--- scripts/3_train/3_memmap_large_allocate.sh
+++ scripts/3_train/3_memmap_large_allocate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-
 python dataset_memmap_large_allocate.py \
-
+  --dataset-tsv "$TSV_FILE" \
+  --embedding-dir "/data/cctm/youn/metaphlan_dset/embeddings/phylophlan_markers/evo" \
+  --memmap-dir "/data/bwh-comppath-seq/youn/metaphlan_dset/model_training/memmap_samples_complete_large" \
+  --dimension-reduce 768
 
 echo "Done!"
