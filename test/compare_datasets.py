c94b29d287e1bd273b2256744d6a3aaaca03b2ff
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-23 12:44:44 -0500

    created scripts which implement alloc + filling of large tdict version of dataset.

--- test/compare_datasets.py
+++ test/compare_datasets.py
@@ -96,6 +96,28 @@ def time_memmap_padded(sample_ids: List[str], memmap_dir: Path, n_iters: int = 1
                 break
 
 
+def time_memmap_large(sample_ids: List[str], memmap_dir: Path, n_iters: int = 100, batch_sz: int = 5):
+    print(f"Using memmap dir: {memmap_dir}")
+    dset = MetaphlanDatasetMemmappedLarge(memmap_dir=memmap_dir)
+
+    rng = torch.Generator()
+    rng.manual_seed(12345)
+
+    dloader = MetaphlanDataLoader(
+        dataset=dset,
+        batch_size=batch_sz, num_workers=8, pin_memory=True,
+        generator=rng, drop_last=False, prefetch_factor=2,
+        persistent_workers=True,
+        shuffle=True,
+        # this doesn't need a collate_fn, since dset implements __getitems__, supported by torch 1.12 onwards
+    )
+    with timer("Tensordict-memmap:large"):
+        for batch_idx, batch in tqdm(enumerate(dloader), total=len(dloader)):
+            x = batch[1].to("cuda", non_blocking=True).sum()
+            if batch_idx == n_iters - 1:
+                break
+
+
 if __name__ == "__main__":
     df = initialize_test_dataset(Path("/data/cctm/youn/metaphlan_dset/model_training/test.tsv"))
 
