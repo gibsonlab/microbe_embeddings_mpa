7ceb8c5de0047f4d454dddec4dbb4eda741776c2
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-28 10:40:44 -0500

    bugfix in main script

--- scripts/3_train/train_model.py
+++ scripts/3_train/train_model.py
@@ -187,7 +187,7 @@ def parse_args() -> argparse.Namespace:
 
     parser.add_argument("-epochs", "--epochs", dest="n_epochs", type=int, required=True)
     parser.add_argument("-sequential", "--sequential", dest="sequential_train", action="store_true", default=False,
-                        help="If flag is given, samples will be shuffled batch-wise. Meaning: batch order will be shuffled, but batch contents will not.")
+                        help="If flag is given, samples will NOT be shuffled.")
     parser.add_argument("-lr", "--learning-rate", dest="lr", type=float, required=True)
     parser.add_argument("-b", "--batch-size", dest="batch_size", type=int, required=True)
 
