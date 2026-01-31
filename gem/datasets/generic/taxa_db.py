ff5f84b244abf162beb05994d80a461b1f7f4086
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-31 09:59:27 -0500

    minor debug messages.

--- gem/datasets/generic/taxa_db.py
+++ gem/datasets/generic/taxa_db.py
@@ -26,6 +26,7 @@ class MetaphlanTaxaDatabase(BacterialTaxaDatabase):
         :param json_index_path: A path to the ZST-compressed JSON index file.
         :param fasta_path: A path to a pre-existing FASTA file containing the raw marker sequences.
         """
+        print("json_index_path: ", json_index_path)
         with zstd.open(json_index_path, "rt") as f:
             sgb_marker_index = json.load(f)
 
