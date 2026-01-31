6807ab1766829336c4dbd06bc25c4b6e5a75f4d7
Younhun Kim <ykim78@bwh.harvard.edu>
2026-01-31 10:36:20 -0500

    debug

--- gem/datasets/generic/taxa_db.py
+++ gem/datasets/generic/taxa_db.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import zstandard as zstd
 import json
 from pyfaidx import Fasta
+from tqdm import tqdm
 
 from .types import *
 
@@ -43,7 +44,7 @@ class MetaphlanTaxaDatabase(BacterialTaxaDatabase):
         self.catalogue: Dict[str, BacterialTaxa] = {}
 
         print(f"Loading marker sequence catalog from {json_index_path}")
-        for sgb_id_numeric_str, seq_record_ids in sgb_marker_index.items():
+        for sgb_id_numeric_str, seq_record_ids in tqdm(sgb_marker_index.items(), desc='Marker-DB', unit='SGB'):
             # remember to attach the "SGB" prefix!
             sgb_id = f'SGB{sgb_id_numeric_str}'
             self.catalogue[sgb_id] = [str(fasta[seq_id]) for seq_id in seq_record_ids]
