14e3f17e7ed9f79c0a03df5a86c513e0c7882c5a
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-02 11:28:17 -0500

    bugfix

--- scripts/human_microbiome_compendium/common/ml/samples.py
+++ scripts/human_microbiome_compendium/common/ml/samples.py
@@ -127,7 +127,8 @@ class MicrobiomeProject:
                         sample_obj = samples[sample_id]
                         asv_count = int(float(abund_token))
                         if asv_count > 0:
-                            print(f"DEBUG: {sample_id}, {asv_id}: {asv_count}")
+                            if "ERS5796433" == sample_id:
+                                print(f"DEBUG: {sample_id}, {asv_id}: {asv_count}")
                             sample_obj.set_count(asv_id, asv_count)
 
 
@@ -143,5 +144,4 @@ class MicrobiomeProject:
             #     else:
             #         print(f"DEBUG: {asv_id}: {asv_count}")
             #         sample_obj.set_count(asv_id, asv_count)
-                raise Exception("debug - stop here")
         return samples
