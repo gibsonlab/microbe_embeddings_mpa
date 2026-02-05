5629b2fb33a026953fa338f49ae502984ea5f699
Younhun Kim <youn.kim.3181@gmail.com>
2026-02-05 16:22:18 -0500

    debug

--- scripts/human_microbiome_compendium/common/ml/samples.py
+++ scripts/human_microbiome_compendium/common/ml/samples.py
@@ -131,6 +131,7 @@ class MicrobiomeProject:
                         if asv_count > 0:
                             sample_obj.set_count(asv_id, asv_count)
 
-        print(samples['ERS3136834'])
+        sid = next(iter(sample_id_subset))
+        print(sid, ":", samples[sid])
         exit(1)
         return [samples[s_id] for s_id in sample_id_subset]
