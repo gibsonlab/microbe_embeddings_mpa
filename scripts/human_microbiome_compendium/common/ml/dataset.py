3edba7f94f37ca353c20904b1d2a351f183a68a2
Younhun Kim <youn.kim.3181@gmail.com>
2026-02-05 16:32:22 -0500

    debug

--- scripts/human_microbiome_compendium/common/ml/dataset.py
+++ scripts/human_microbiome_compendium/common/ml/dataset.py
@@ -68,6 +68,8 @@ class MicrobiomeSampleEmbedding:
                 for asv_idx, asv_id in enumerate(asv_ids):
                     features[asv_idx] = h5_file[asv_id][:]
 
+        print("sample: {}".format(sample.sample_id))
+        print("asv_ids = {}".format(asv_ids))
         return asv_ids, features, abunds
 
     def get_abundance_profile_lightweight(self, sample: MicrobiomeSample) -> Tuple[List[str], np.ndarray]:
