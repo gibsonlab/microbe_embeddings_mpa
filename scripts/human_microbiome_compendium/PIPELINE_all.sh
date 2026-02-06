a55aab19522d27587e2d89979204a7908af06a9a
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-06 12:06:03 -0500

    unified 3_train_model.sh -- now takes an argument that specifies model name.

--- scripts/human_microbiome_compendium/PIPELINE_all.sh
+++ scripts/human_microbiome_compendium/PIPELINE_all.sh
@@ -20,7 +20,7 @@ fi
 
 for embed_name in "dnabert-s" "evo2_7b_hyena10" "evo-1-8k-base_hyena5"; do
   bash 2_embed.sh "$embed_name" "$dset_name"
-  bash 3_train_model_epc.sh "$embed_name" "$dset_name"
-  bash 3_train_model_epc_nopool.sh "$embed_name" "$dset_name"
+  bash 3_train_model.sh "$embed_name" "epc_pool" "$dset_name"
+  bash 3_train_model.sh "$embed_name" "epc_nopool" "$dset_name"
 done
 
