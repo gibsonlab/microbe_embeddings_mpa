6f617ce7696631ff2a991272418119871b13dd7c
Younhun Kim <youn.kim.3181@gmail.com>
2026-02-12 14:14:44 -0500

    update PIPELINE_all.sh to suggest extra help messages.

--- scripts/human_microbiome_compendium/PIPELINE_all.sh
+++ scripts/human_microbiome_compendium/PIPELINE_all.sh
@@ -10,7 +10,7 @@ if [ $# -eq 0 ]; then
   for dir in "${BASEDIR}"/*; do
     echo "-> $(basename "${dir}")"
   done
-  echo "Run this command "
+  echo "Run this command with 'bash PIPELINE_all.sh <dataset>' to print the available analysis subdirectories."
   exit 1
 fi
 dset_name="$1"
