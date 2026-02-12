bc0717ff6ab2a3dbb52a518484c8318d181a8509
Younhun Kim <youn.kim.3181@gmail.com>
2026-02-12 14:13:42 -0500

    update PIPELINE_all.sh to suggest extra help messages.

--- scripts/human_microbiome_compendium/PIPELINE_all.sh
+++ scripts/human_microbiome_compendium/PIPELINE_all.sh
@@ -2,22 +2,55 @@
 set -e
 
 BASEDIR="/data/bwh-comppath-seq/youn/human_microbiome_compendium"
-if ! [ $# -eq 2 ]; then
+# Suggest dataset subdir names.
+if [ $# -eq 0 ]; then
   echo "Error: dataset is required"
   echo "Usage: $0 <dataset> <analysis>"
   echo "Available dataset names:"
   for dir in "${BASEDIR}"/*; do
     echo "-> $(basename "${dir}")"
   done
+  echo "Run this command "
   exit 1
 fi
 dset_name="$1"
-analysis_name="$2"
 
 if ! [ -d "${BASEDIR}/${dset_name}" ]; then
   echo "Dataset files for '${dset_name}' do not exist!"
   exit 1
 fi
+
+# Suggest analysis subdir names.
+print_available_analyses() {
+  _dset_name=$1
+  subdir="${BASEDIR}/${dset_name}/analyses"
+
+  echo "Available analyses for dataset '${dset_name}':"
+  analyses_base="${BASEDIR}/${dset_name}/analyses"
+  if [ -d "${analyses_base}" ]; then
+    for dir in "${analyses_base}"/*; do
+      if [ -d "${dir}" ]; then
+        echo "-> $(basename "${dir}")"
+      fi
+    done
+  else
+    echo "No analyses/ directory found for this dataset"
+  fi
+}
+
+if [ $# -eq 1 ]; then
+  print_available_analyses "$dset_name"
+  exit 1
+fi
+
+analysis_name="$2"
+ANALYSIS_DIR="${BASEDIR}/${dset_name}/analyses/${analysis_name}"
+if ! [ -d "${ANALYSIS_DIR}" ]; then
+  echo "Analysis subdir does not exist: ${ANALYSIS_DIR}"
+  print_available_analyses "$dset_name"
+  exit 1
+fi
+
 
 for embed_name in "dnabert-s" "evo2_7b_hyena10" "evo-1-8k-base_hyena5"; do
   bash 2_embed.sh "$embed_name" "$dset_name"
