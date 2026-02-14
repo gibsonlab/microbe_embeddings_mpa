19836f242e4f5e90381d627da67cec2be6d5633e
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-14 02:34:25 -0500

    added slurm script for PIPELINE_all.

---
+++ scripts/human_microbiome_compendium/PIPELINE_all_slurm.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -e
+
+
+BASEDIR="/data/bwh-comppath-seq/youn/human_microbiome_compendium"
+# Suggest dataset subdir names.
+if [ $# -eq 0 ]; then
+  echo "Error: dataset is required"
+  echo "Usage: $0 <dataset> <analysis>"
+  echo "Available dataset names:"
+  for dir in "${BASEDIR}"/*; do
+    echo "-> $(basename "${dir}")"
+  done
+  echo "Run this command with 'bash PIPELINE_all.sh <dataset>' to print the available analysis subdirectories."
+  exit 1
+fi
+dset_name="$1"
+
+dset_dir="${BASEDIR}/${dset_name}"
+EXCLUDE_NODES=""
+
+# Get list of currently running/pending jobs for this user
+running_jobs=$(squeue -u $USER -h -o "%j" 2>/dev/null)
+
+SCRIPT_DIR="./slurm/${dset_name}"
+LOG_DIR="${SCRIPT_DIR}/logs"
+mkdir -p "${SCRIPT_DIR}"
+mkdir -p ${LOG_DIR}
+
+# Loop through all analysis subdirectories
+for analysis_subdir in "${dset_dir}"/analyses/*; do
+    if ! [ -d "${analysis_subdir}" ]; then
+        echo "${analysis_subdir} is not a directory. Skipping."
+        continue
+    fi
+
+    # Extract just the directory name (e.g., LOO_PRJNA391858)
+    analysis_name=$(basename "${analysis_subdir}")
+
+    # Check if a job with this name is already running or pending
+    if echo "$running_jobs" | grep -q "^${analysis_name}$"; then
+        echo "Skipping ${analysis_name} - job already running/pending."
+        continue
+    fi
+
+    # Check if the job already finished previously.
+    BREADCRUMB_FILE="${LOG_DIR}/${analysis_name}.DONE"
+    if [ -f "${BREADCRUMB_FILE}" ]; then
+        echo "Skipping ${analysis_name} - job already finished."
+        continue
+    fi
+
+    # Create a job script for this subdirectory
+    jobname="${dset_name}:${analysis_name}"
+    job_script="${SCRIPT_DIR}/${jobname}.sh"
+    rm -f "${LOG_DIR}/${jobname}.out"
+    rm -f "${LOG_DIR}/${jobname}.err"
+
+    # Write SBATCH headers and job commands
+    cat > "${job_script}" << EOF
+#!/bin/bash
+#SBATCH --partition=bwh_comppath_all
+#SBATCH --gpus=1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=8
+#SBATCH --time=12:00:00
+#SBATCH --job-name=${analysis_name}
+#SBATCH --output=${LOG_DIR}/${jobname}.out
+#SBATCH --error=${LOG_DIR}/${jobname}.err
+
+# Run the pipeline
+bash PIPELINE_all.sh "${dset_name}" "${analysis_name}"
+touch "${BREADCRUMB_FILE}"
+EOF
+
+    # Submit the job
+    sbatch --exclude="${EXCLUDE_NODES}" "${job_script}"
+
+    echo "Submitted job for dset ${dset_name}, analysis ${analysis_name}"
+done
+
