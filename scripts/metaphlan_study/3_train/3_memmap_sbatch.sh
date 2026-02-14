d447969e3e4f0c600dcfd113836063ffc527aae0
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-14 05:46:34 -0500

    created bash script for sbatch submission.

--- scripts/metaphlan_study/3_train/3_memmap_sbatch.sh
+++ scripts/metaphlan_study/3_train/3_memmap_sbatch.sh
@@ -4,5 +4,5 @@
 for embed_model in "dnabert-s" "evo-1-8k-base_hyena5" "evo2_7b_hyena10" ; do
   logdir="./logs/memmap/${embed_model}"
   mkdir -p "${logdir}"
-  sbatch --output="${logdir}/memmap_%A_%a.out" --error="${logdir}/memmap_%A_%a.err" 3_memmap.sh "${embed_model}"
+  sbatch --job-name="memmap:${embed_model}" --output="${logdir}/memmap_%A_%a.out" --error="${logdir}/memmap_%A_%a.err" 3_memmap.sh "${embed_model}"
 done
