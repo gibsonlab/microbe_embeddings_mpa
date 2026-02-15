612c10f4b39c91adda63971b80985bb63305fabb
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-15 15:14:27 -0500

    bugfix

--- scripts/metaphlan_study/0_download/create_sgb_subset_file.py
+++ scripts/metaphlan_study/0_download/create_sgb_subset_file.py
@@ -4,26 +4,13 @@ import pandas as pd
 from gem.datasets import MetaphlanProfileParser
 
 
-def select_profiles_in_metadata(metadata_df, profiles_df) -> pd.DataFrame:
-    return profiles_df.loc[profiles_df.index.isin(metadata_df['Sample ID'])]
-
-
 def main(
         profile_tsv: Path,
-        metadata_tsv: Path,
+        out_path: Path,
 ):
     profiles = pd.read_csv(profile_tsv, sep="\t")
     profiles_indexed = profiles.set_index("clade_name").transpose()
     profiles_indexed.index.name = "SampleID"
-
-    # metadata = pd.read_csv(metadata_tsv, sep="\t")
-    # print("Number of samples (All): {}".format(metadata.shape[0]))
-    # metadata_subset = metadata.loc[
-    #     (metadata['age_category'] == 'adult')
-    #     & (metadata['disease'] == 'healthy')
-    # ]
-
-    # profile_df = select_profiles_in_metadata(metadata_subset, profiles_indexed)
     extractor = MetaphlanProfileParser(profiles_indexed)
 
     sgb_ids_subset = set()
@@ -32,17 +19,22 @@ def main(
         sgb_ids_subset.update(sample.taxa_ids_raw)
         n_samples += 1
 
-    print("# of samples in healthy-adult subset: {}".format(
+    print("# of samples in study: {}".format(
         n_samples
     ))
-    print("Found: {} SGBs in healthy-adult sample subset.".format(
+    print("# of SGBs found in study: {}".format(
         len(sgb_ids_subset)
     ))
+
+    with open(out_path, "w") as f:
+        for sgb_id in sorted(sgb_ids_subset):
+            print(sgb_id, file=f)
+    print("Wrote SGB IDs to {}".format(out_path))
 
 
 if __name__ == '__main__':
     data_dir = Path("/data/bwh-comppath-seq/youn/metaphlan_dset/dataset")
     main(
         profile_tsv=data_dir / "BlancoMiguezA_2023_profiles.tsv",
-        metadata_tsv=data_dir / "BlancoMiguezA_2023_metadata.tsv",
+        out_path=data_dir / "BlancoMiguezA_2023.SGB_subset.txt",
     )
