1ff47572d8bb04b5a146ca14162c3c7888454255
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-15 22:46:05 -0500

    bugfix

--- scripts/metaphlan_study/2_embed/compute_offline_embeddings.py
+++ scripts/metaphlan_study/2_embed/compute_offline_embeddings.py
@@ -78,7 +78,7 @@ def store_embeddings(embeddings: np.ndarray, sgb_id_order: List[str], out_path: 
     for sgb_idx, (sgb_id, sgb_embedding) in enumerate(zip(sgb_id_order, embeddings)):
         memmap_array[sgb_idx, 0, :] = sgb_embedding
 
-    with open(out_path.with_suffix("meta"), "wt") as f:
+    with open(out_path.with_suffix(".meta"), "wt") as f:
         print("float32", file=f)
         print(','.join(memmap_shape), file=f)
 
