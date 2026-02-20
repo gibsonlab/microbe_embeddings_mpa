afb991be2c6f3bee2fcf4a33c7b62bf84c2a02ea
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-19 20:18:35 -0500

    added tqdm to ipca loop.

---
+++ scripts/metaphlan_study/2_embed/2_ipca_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=bwh_comppath
+#SBATCH --ntasks=1
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=80
+#SBATCH --time=12:00:00
+#SBATCH --job-name=ipca
+#SBATCH --output=logs/ipca_%j.out
+#SBATCH --error=logs/ipca_%j.err
+
+
+python 2_ipca_dimreduce.py
+echo "Done."
