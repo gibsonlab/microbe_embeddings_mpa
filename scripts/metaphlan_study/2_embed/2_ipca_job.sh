6df079c968640ded77812db45efa1756931af441
Younhun Kim <ykim78@bwh.harvard.edu>
2026-02-19 20:22:16 -0500

    added tqdm to ipca loop.

--- scripts/metaphlan_study/2_embed/2_ipca_job.sh
+++ scripts/metaphlan_study/2_embed/2_ipca_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=200G
 #SBATCH --cpus-per-task=80
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=ipca
 #SBATCH --output=logs/ipca_%j.out
 #SBATCH --error=logs/ipca_%j.err
