d4b4717d1bd65de9058b44703af8f462754bd4c3
Younhun Kim <ykim78@bwh.harvard.edu>
2026-03-07 09:14:46 -0500

    reverted experimental changes to batch size.

--- scripts/metaphlan_study/3_train_numpy_memmap/2_train.sh
+++ scripts/metaphlan_study/3_train_numpy_memmap/2_train.sh
@@ -37,10 +37,10 @@ pred_model="$4"
 if [ $embed_family == "offline" ]; then
   embedding_file="${EMBEDDING_BASE_DIR}/${embed_family}/${embed_model}.pt"
   # deep-learning embeddings are much larger; can't accommodate large batch sizes.
-  batch_size=10
+  batch_size=30
 else
   embedding_file="${EMBEDDING_BASE_DIR}/${embed_family}/${embed_model}.ipca_200.pt"
-  batch_size=10
+  batch_size=30
 fi
 echo "Input embedding file: ${embedding_file}"
 
