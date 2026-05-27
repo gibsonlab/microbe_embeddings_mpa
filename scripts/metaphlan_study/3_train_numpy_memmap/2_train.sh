248bf4ff67555d6435ef9badaf98609aabbb487d
Younhun Kim <youn.kim.3181@gmail.com>
2026-05-26 21:22:18 -0400

    reverted to ISAB implementation, as SAB was encountering OOM issues. Raised n_inds to 16.

--- scripts/metaphlan_study/3_train_numpy_memmap/2_train.sh
+++ scripts/metaphlan_study/3_train_numpy_memmap/2_train.sh
@@ -38,10 +38,10 @@ seed="$5"
 if [ $embed_family == "offline" ]; then
   embedding_file="${EMBEDDING_BASE_DIR}/${embed_family}/${embed_model}.pt"
   # deep-learning embeddings are much larger; can't accommodate large batch sizes.
-  batch_size=10
+  batch_size=30
 else
   embedding_file="${EMBEDDING_BASE_DIR}/${embed_family}/${embed_model}.ipca_d200.pt"
-  batch_size=10
+  batch_size=30
 fi
 echo "Input embedding file: ${embedding_file}"
 
